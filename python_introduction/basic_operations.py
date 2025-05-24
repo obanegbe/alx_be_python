27229921542b20147926ce7c70183d69ec487162
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-05-24 04:07:24 +0100

    Basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number2 + number1
+difference = number1 - number2
+product = number2 * number1
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
