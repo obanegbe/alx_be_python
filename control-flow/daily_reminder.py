293054b5acdad19362a63ec41c76f4c4e004a21d
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-01 17:00:24 +0100

    update daily reminder app again

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,13 +4,11 @@ priority = input(" task’s priority (high, medium, low): ")
 
 match priority:
     case "high":
-        message = f"'{task}' is a {priority} priority task"
+        message = f"'{task}' is a high priority task"
     case "medium":
-        message = f"'{task}' is a {priority} priority task"
+        message = f"'{task}' is a medium priority task"
     case "low":
-        message = f"'{task}' is a {priority} priority task"
-    case _:
-        message = f"'{task}' is an unknown priority task"
+        message = f"'{task}' is a low priority task"
 
 if time_bound == "yes":
     message += " that requires immediate attention today!"
