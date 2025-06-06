cf05270c14cbd26d70490654b0b8c54ee5f0c729
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-06 14:15:13 +0100

    Update2 Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,6 +12,6 @@ def perform_operation(num1, num2, operation):
 
         case "divide":
             if num2 == 0:
-                return "Invalid operation"
-            else:
+                return "Cannot divide by zero"
+            elif num2 != 0:
                 return (num1/num2)
