fdabfc07cc1b2f5d55085c128f0f770618481d33
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-06 13:48:17 +0100

    Add Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operation):
+    
+    match operation:
+        case "+":
+            return (num1 + num2)
+
+        case "-":
+            return (num1 - num2)
+
+        case "*":
+            return (num1 * num2)
+
+        case "/":
+            if num2 == 0:
+                return "Invalid operation"
+            else:
+                return (num1/num2)
