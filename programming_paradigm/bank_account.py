8244efb12f60a9aac1805c610e184688f6c09cea
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-13 17:49:12 +0100

    Add Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    
+    def __init__(self, account_balance=0.0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            print(self.account_balance)
+        else:
+            print("Deposite amount must be greater than zero")
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            print("You do not have up to that in your account balance")
+            return False
+        elif amount <= 0:
+            print("Withdraw amount must be greater than zero")
+            return False
+        else:
+            self.account_balance -= amount
+            print(self.account_balance)
+
+    def display_balance(self):
+        print(f"Your account balance is: ${self.account_balance}")
