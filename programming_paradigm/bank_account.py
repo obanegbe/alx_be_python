66f09222320a96653a1e9b6037435fe1e2a81d5e
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-13 19:42:50 +0100

    Create a simple bank account class update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     
-    def __init__(self, account_balance=0.0):
+    def __init__(self, account_balance):
         self.account_balance = account_balance
 
     def deposit(self, amount):
