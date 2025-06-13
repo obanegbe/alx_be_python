d7fcfbde5e0512ffa90d4abc8b492af9294d5d4e
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-13 18:19:20 +0100

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -22,4 +22,4 @@ class BankAccount:
             print(self.account_balance)
 
     def display_balance(self):
-        print(f"Your account balance is: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
